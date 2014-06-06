563b2e332d0c8290452c6e55be228f4c590d37a2
beverly mah <bjm328@nyu.edu>
2014-06-06 19:47:16 -0400

    added new and create actions in comments controller

--- dbcoverflow/app/controllers/comments_controller.rb
+++ dbcoverflow/app/controllers/comments_controller.rb
@@ -2,16 +2,20 @@ class CommentController < ApplicationController
   def index
   end
 
+  def show
+  end
 
   #this is going to render the comment form
   def new
     @comment = Comment.new
   end
 
-  def show
-  end
-
+  ##the comment form is going to post to this method.
+  ##create the comment and link to user
   def create
+    user = User.find(session[:user_id])
+    user.comments.create(params[:comment])
+
   end
 
   def edit
