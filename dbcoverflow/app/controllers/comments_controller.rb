6a66a44c3a137cd748a48886a12911e6b0b94ea6
beverly mah <bjm328@nyu.edu>
2014-06-06 19:41:30 -0400

    add comments controller

---
+++ dbcoverflow/app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentController < ApplicationController
+  def index
+  end
+
+
+  #this is going to render the comment form
+  def new
+    @comment = Comment.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
