fc319ffaea93c0af982ff760eb5104a045600773
justinphelps <justin.phelps@gmail.com>
2014-06-06 16:35:51 -0400

    added vote tests

--- dbcoverflow/spec/models/vote_spec.rb
+++ dbcoverflow/spec/models/vote_spec.rb
@@ -1,8 +1,23 @@
-require 'spec_helper'
-
 describe Vote do
-	
 
+	context "migrations" do
 
-	
+		let(:vote) { FactoryGirl.build :vote}
+
+		it { vote.should respond_to(:score)}
+		it { vote.should respond_to(:user)}
+		it { vote.should respond_to(:votable)}
+	end
+
+	context "validations" do
+		it { should validate_presence_of :score }
+		it { should validate_presence_of :user }
+		it { should validate_presence_of :votable }
+	end
+
+	context "assocations" do
+		it { should belong_to(:user) }
+		it { should belong_to(:votable) }		
+	end
+
 end
