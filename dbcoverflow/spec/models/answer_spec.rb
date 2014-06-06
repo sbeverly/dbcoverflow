6a54a70cabd0baca72ce5d58114ce38a12500bda
nealfennimore <fennimoren@gmail.com>
2014-06-06 15:58:11 -0400

    Added context fields for rspec sorting

--- dbcoverflow/spec/models/answer_spec.rb
+++ dbcoverflow/spec/models/answer_spec.rb
@@ -7,22 +7,25 @@ describe Answer do
   answer.question = question
   answer.user = user
   answer.save
-  p answer
+  # p answer
   let (:answer) { answer }
 
 # ========= Field classes
+context "instances" do
   it { answer.should be_an_instance_of(Answer)}
   it { answer.user.should be_an_instance_of(User)}
   it { answer.question.should be_an_instance_of(Question)}
   it { answer.body.should be_an_instance_of(String)}
-
+end
   # it { answer.body.should eq "Random question" }
 
 # ========= Responses on migrations
+context "validations" do
   it { answer.should respond_to(:body) } 
   it { answer.should respond_to(:question) }
   it { answer.should respond_to(:user) }
   it { answer.should have_many :votes}
   it { answer.should have_many :comments}
+end
 	
 end
