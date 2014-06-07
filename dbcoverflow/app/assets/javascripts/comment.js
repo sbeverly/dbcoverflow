237bd5f42b2c40166e14e4dbaf381bcaed788050
Bev <beverlymah@gmail.com>
2014-06-06 21:23:34 -0400

    work on javascript for comment

--- dbcoverflow/app/assets/javascripts/comment.js
+++ dbcoverflow/app/assets/javascripts/comment.js
@@ -1,8 +1,32 @@
+// ##this will append the "comemnt" form on either question
+// or answer
+
 $(document).ready() {
 	$('#question_comment_link').on('click', function(e){
 		e.preventDefault();
+
+		// ##grab the form partial from the comment controller 
 		$.ajax({
-			url: '/comments_controller/'
+			url: '/comments_controller/new'
+			method: 'GET'
+		}).done(function(response) {
+		// ##somehow grab that form partial and append it to 
+		// the end of the question div
+			$('#question div').append(response)
+		})
+	})
+
+	$('#answer_comment_link').on('click', function(e) {
+		e.preventDefault();
+
+		$.ajax({
+			url: '/comments_controller/new'
+			method: 'GET'
+		}).done(function(response) {
+			$('#answer div').append(response)
 		})
 	})
 }
+
+
+
