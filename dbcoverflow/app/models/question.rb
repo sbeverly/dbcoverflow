f862b0b284e1413f2b562ba8cc3b4c8bb941acfe
Siyan Beverly <siyan.beverly@gmail.com>
2014-06-07 13:08:52 -0400

    change form_for on index page to use short form syntax

--- dbcoverflow/app/models/question.rb
+++ dbcoverflow/app/models/question.rb
@@ -3,4 +3,9 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable
   has_many :answers
+
+  # Need to confirm with Alex how the session hash is populated so that I can match user id with the 
+  def validate_question_ownership(body)  	
+  	@question.user_id === session[:id] ? true : false
+  end
 end
