48f588cc08c98e3357c5063e83a838a2d1082e04
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-07 14:33:09 -0400

    updated user features spec

--- dbcoverflow/spec/features/users_spec.rb
+++ dbcoverflow/spec/features/users_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe "User" do
 
-	context "can create a new account with VALID attr" do
-
 		# before :each do
 		# 	visit root_url
 		# 	click_link "Create New User"
@@ -13,14 +11,16 @@ describe "User" do
 			visit new_user_url
 		end
 
+	context "can create a new account with VALID attr" do
+
 		it "by navigating to the user sign up page" do
-			expect(page.current).to eq new_user_url
+			expect(page.current_url).to eq new_user_url
 		end
 
 		it "by submitting valid user credentials" do
 			fill_in 'Username', with: 'ExampleUsername'
 			fill_in 'Password', with: 'password'
-			fill_in 'Password Confirmation', with: "password"
+			fill_in 'Password confirmation', with: "password"
 			fill_in "Email", with: 'example@yahoo.com'
 			click_button 'Create User'
 
@@ -33,55 +33,55 @@ describe "User" do
 
 		after :each do
 			click_button 'Create User'
-			expect(page.current_url).to eq(user_url(user))
+			expect(page.current_url).to eq(new_user_url)
 		end
 
 		it "by creating a new user with no username" do
 			fill_in 'Username', with: ''
 			fill_in 'Password', with: 'password'
-			fill_in 'Password Confirmation', with: 'password'
+			fill_in 'Password confirmation', with: 'password'
 			fill_in "Email", with: 'example@yahoo.com'
 		end
 
 		it "by creating a new user with no email address" do
 			fill_in 'Username', with: 'ExampleUsername'
 			fill_in 'Password', with: 'password'
-			fill_in 'Password Confirmation', with: 'password'
+			fill_in 'Password confirmation', with: 'password'
 			fill_in "Email", with: ''
 		end
 
 		it "by creating a new user with no password" do
 			fill_in 'Username', with: 'ExampleUsername'
 			fill_in 'Password', with: ''
-			fill_in 'Password Confirmation', with: ''
+			fill_in 'Password confirmation', with: ''
 			fill_in "Email", with: 'example@yahoo.com'
 		end
 
 		it "by submitting a username less than 3 chars" do
 			fill_in 'Username', with: 'Ex'
 			fill_in 'Password', with: 'password'
-			fill_in 'Password Confirmation', with: 'password'
+			fill_in 'Password confirmation', with: 'password'
 			fill_in "Email", with: 'example@yahoo.com'
 		end
 
 		it "by submitting a password less than 6 chars" do
 			fill_in 'Username', with: 'ExampleUsername'
 			fill_in 'Password', with: 'pass'
-			fill_in 'Password Confirmation', with: 'pass'
+			fill_in 'Password confirmation', with: 'pass'
 			fill_in "Email", with: 'example@yahoo.com'
 		end
 
 		it "by submitting an invalid email address" do
 			fill_in 'Username', with: 'ExampleUsername'
 			fill_in 'Password', with: 'password'
-			fill_in 'Password Confirmation', with: 'password'
+			fill_in 'Password confirmation', with: 'password'
 			fill_in "Email", with: 'bad_example.com'
 		end
 
 		it "by submitting an invalid password confirmation" do
 			fill_in 'Username', with: 'ExampleUsername'
 			fill_in 'Password', with: 'password'
-			fill_in 'Password Confirmation', with: 'not'
+			fill_in 'Password confirmation', with: 'not'
 			fill_in "Email", with: 'example@yahoo.com'
 		end
 
