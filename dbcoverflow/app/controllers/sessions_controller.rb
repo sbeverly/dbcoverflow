55201781144e697a6004f78b738b346cc5f6129f
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-08 13:34:19 -0400

    add session functionality in session controller

--- dbcoverflow/app/controllers/sessions_controller.rb
+++ dbcoverflow/app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+	def create
+		if user = User.authenticate(params[:username], params[:password])
+			session[:current_user_id] = user.id
+			redirect_to questions_url
+		end
+	end
+
+	def destroy
+		@current_user = session[:current_user_id] = nil
+		redirect_to questions_url
+	end
+
 end
