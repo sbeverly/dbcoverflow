bd2e958c6b96a4eaca0c1a0146a5b32f33cf2d6b
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-08 12:21:48 -0400

    added votes feature testing

--- dbcoverflow/spec/features/votes_spec.rb
+++ dbcoverflow/spec/features/votes_spec.rb
@@ -2,7 +2,37 @@ require 'spec_helper'
 
 describe "Votes" do
 	
+	before :each do
+		visit questions_url
+		@question = FactoryGirl.create(:question)
+		@answer = FactoryGirl.create(:answer)
+		@question.answers << @anwer
+		click_link @question.body
+	end
 
+	describe "voting on questions" do
 
+		it "by voting up" do
+			click_link "q: Vote Up"
+			expect(page).to have_content("q: 1")
+		end
+
+		it "by voting down" do
+			click_link "q: Vote Down"
+			expect(page).to have_content("q: -1")
+		end
+	end
+
+	describe "voting on answers" do
+		it "by voting up" do
+			click_link "a: Vote Up"
+			expect(page).to have_content("a: 1")
+		end
+
+		it "by voting down" do
+			click_link "q: Vote Down"
+			expect(page).to have_content("a: -1")
+		end
+	end
 	
 end
