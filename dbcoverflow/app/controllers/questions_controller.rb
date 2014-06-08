2ce1e3c28bceae8981b8b67787ae162950054e21
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-08 16:27:59 -0400

    removed logged_in? method from user controller

--- dbcoverflow/app/controllers/questions_controller.rb
+++ dbcoverflow/app/controllers/questions_controller.rb
@@ -18,6 +18,7 @@ class QuestionsController < ApplicationController
 
 	def create
 		@question = Question.create(body: params[:question][:body])
+		@question.vote = Vote.create
 		redirect_to root_path
 	end
 
@@ -38,22 +39,14 @@ class QuestionsController < ApplicationController
 	end
 
 	def upvote
-		if logged_in?
+			@question.vote.score += 1
 			flash[:notice] = "your upvote has been recorded"
-			@question.votes << Vote.create(user_id: session[:user_id])
 			redirect_to question_path(@question)
-		else
-			flash[:alert] = "You must be logged in to vote."
-			redirect_to question_path(@question)
-		end
 	end
 
 	def downvote
-		if logged_in?
 			flash[:notice] = "your downvote has been recorded"
 			redirect_to question_path(@question)
-		end
-
 	end
 
 	private
@@ -66,11 +59,4 @@ class QuestionsController < ApplicationController
 		@question = Question.find(params[:id])
 	end
 
-	def logged_in?
-		if session[:user_id]
-			return true
-		else
-			return false
-		end
-	end
 end
