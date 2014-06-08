5e49783f7e8c4d6e62521faa7c939f8e977ec125
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-08 15:33:42 -0400

    added upvote and downvote routes for questions

--- dbcoverflow/config/routes.rb
+++ dbcoverflow/config/routes.rb
@@ -12,6 +12,9 @@ Dbcoverflow::Application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
+  post '/questions/:id/upvote' => 'questions#upvote', :as => question_upvote
+  post '/questions/:id/downvote' => 'questions#downvote', :as => question_downvote
+
 end
 
 
