5cdc03e710be41f0c349ccf8301e440d7831d345
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-08 19:01:46 -0400

    fixed answers controller

--- dbcoverflow/app/controllers/answers_controller.rb
+++ dbcoverflow/app/controllers/answers_controller.rb
@@ -31,6 +31,6 @@ class AnswersController < ApplicationController
   private
 
   def find_question
-    @question = Question.find(params[:id])
+    @question = Question.find(params[:question_id])
   end
 end
