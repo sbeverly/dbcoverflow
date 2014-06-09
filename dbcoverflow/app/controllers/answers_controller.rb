cbf7bf8ae1beaeada5c48ad1807aca054f2ab839
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-09 00:24:14 -0400

    add upvote downvote functionality to answers controller

--- dbcoverflow/app/controllers/answers_controller.rb
+++ dbcoverflow/app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   before_action :find_question, only: [:create, :upvote, :downvote]
+  before_action :find_answer, only: [:upvote, :downvote]
 
   def create
     @answer = Answer.create(user_id: session[:user_id], question_id: @question.id, body: params[:answer][:body])
@@ -15,22 +16,24 @@ class AnswersController < ApplicationController
   end
 
   def upvote
-    # puts params
-    # @answer = Answer.find(params[:answer_id])
-    # @answer.vote.score += 1
-    # @answer.vote.save
-    # redirect_to question_path(@question)
+    @answer.vote.score += 1
+    @answer.vote.save
+    redirect_to question_path(@question)
   end
 
   def downvote
-    # @answer.vote.score -= 1
-    # @answer.vote.save
-    # redirect_to question_path(@question)
+    @answer.vote.score -= 1
+    @answer.vote.save
+    redirect_to question_path(@question)
   end
 
   private
 
   def find_question
     @question = Question.find(params[:question_id])
+  end
+
+  def find_answer
+    @answer = Answer.find(params[:answer_id])
   end
 end
